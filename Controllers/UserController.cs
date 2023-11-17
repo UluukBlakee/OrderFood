2a05f875733eda9c05502517c8a1d26af869868b
Uluk <talantbuluk@gmail.com>
2023-11-17 15:50:08 +0600

    Added User Controller and Create, GiveRole, Delete, Index actions

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using OrderFood.Models;
+using OrderFood.ViewModels;
 using System.Data;
 
 namespace OrderFood.Controllers
@@ -22,6 +23,7 @@ namespace OrderFood.Controllers
         public async Task<IActionResult> Index()
         {
             List<User> users = await _context.Users.ToListAsync();
+            ViewBag.UserManager = _userManager;
             return View(users);
         }
         [Authorize(Roles = "admin")]
@@ -29,21 +31,63 @@ namespace OrderFood.Controllers
         {
             return View();
         }
-
+        [HttpPost]
+        public async Task<IActionResult> Create(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var existingUser = await _userManager.FindByEmailAsync(model.Email);
+                if (existingUser != null)
+                {
+                    ModelState.AddModelError("Email", "Пользователь с такой почтой уже зарегистрирован.");
+                    return View(model);
+                }
+                User user = new User
+                {
+                    Email = model.Email,
+                    UserName = model.Email
+                };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(user, "user");
+                    return RedirectToAction(nameof(Index));
+                }
+                foreach (var error in result.Errors)
+                    ModelState.AddModelError(string.Empty, error.Description);
+            }
+            return View(model);
+        }
         [Authorize(Roles = "admin")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            User user = await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+            return View(user);
+        }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(User user)
+        public async Task<ActionResult> DeleteConfirmed(int id)
         {
-            return RedirectToAction("Details", new { id = user.Id });
+            User user = await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+            if (user != null)
+            {
+                IdentityResult result = await _userManager.DeleteAsync(user);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
         }
 
         [Authorize(Roles = "admin")]
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult GiveRole(User user)
+        public async Task<ActionResult> GiveRole(int id)
         {
-            return RedirectToAction("Details", new { id = user.Id });
+            User user = await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+            if (user != null)
+            {
+                await _userManager.AddToRoleAsync(user, "admin");
+                return RedirectToAction(nameof(Index));
+            }
+            return RedirectToAction(nameof(Index));
         }
     }
 }
