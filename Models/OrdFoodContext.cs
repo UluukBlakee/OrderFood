6ab421f75f1c0a141d9d740c0ce44772c967b7d6
Uluk <talantbuluk@gmail.com>
2023-11-17 11:51:40 +0600

    Initial

---
+++ Models/OrdFoodContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace OrderFood.Models
+{
+    public class OrdFoodContext : IdentityDbContext<User, IdentityRole<int>, int>
+    {
+        public DbSet<User> Users { get; set; }
+        public OrdFoodContext(DbContextOptions<OrdFoodContext> options) : base(options) { }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using OrderFood.Models;
+
+namespace OrderFood
+{
+    public class Program
+    {
+        public static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
+            string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<OrdFoodContext>(options => options.UseNpgsql(connection)).AddIdentity<User, IdentityRole<int>>(options =>
+            {
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireDigit = false;
+            })
+                .AddEntityFrameworkStores<OrdFoodContext>();
+
+            var app = builder.Build();
+
+            var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
+            using (var scope = scopeFactory.CreateScope())
+            {
+                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole<int>>>();
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
+                await AdminInitializer.SeedAdminUser(roleManager, userManager);
+            }
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
