aae4014a0f46582e6b74a0a52cb3f4ba41748815
Uluk <talantbuluk@gmail.com>
2023-11-17 12:02:39 +0600

    Add services and Cafe, Dish Models

---
+++ Models/Dish.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderFood.Models
+{
+    public class Dish
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ Services/AdminInitializer.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using OrderFood.Models;
+
+namespace OrderFood.Services
+{
+    public class AdminInitializer
+    {
+        public static async Task SeedAdminUser(RoleManager<IdentityRole<int>> roleManager, UserManager<User> userManager)
+        {
+            string adminEmail = "admin@admin.com";
+            string adminPassword = "1qwe@QWE";
+            var roles = new[] { "admin", "user" };
+            foreach (var r in roles)
+            {
+                if (await roleManager.FindByNameAsync(r) is null)
+                    await roleManager.CreateAsync(new IdentityRole<int>(r));
+            }
+            if (await userManager.FindByEmailAsync(adminEmail) == null)
+            {
+                User admin = new User { Email = adminEmail, UserName = adminEmail };
+                var result = await userManager.CreateAsync(admin, adminPassword);
+                if (result.Succeeded)
+                    await userManager.AddToRoleAsync(admin, "admin");
+            }
+        }
+    }
+}
